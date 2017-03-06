2fd3e092ac59ec4f264db838ee79b94fbde5f910
katakras <katakrask@gmail.com>
2017-03-06 02:40:11 +0000

    first steps of matrix class
    access, constructors

--- include/Matrix.h
+++ include/Matrix.h
@@ -1,8 +1,27 @@
 
+#ifndef _MATRIX_
+#define _MATRIX_
+
 class Matrix
 {
-public:
-  Matrix();
 private:
-  double** array;
+  class aVector
+  {
+  public:
+    aVector(double*);
+    double operator[](int);
+  private:
+    double* elements;
+    double n;
+  };
+public:
+  Matrix(int,int);
+  Matrix(const Matrix&);
+  ~Matrix();
+  aVector operator[](int);
+private:
+  double** _array;
+  int _n, _m;
 };
+
+#endif
--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -1,8 +1,46 @@
 
 #include "../include/Matrix.h"
 
-Matrix::Matrix()
+Matrix::Matrix(const Matrix& matrix)
+{
+  _n = matrix._n;
+  _m = matrix._m;
+  _array = new double*[_n];
+  for(int i = 0; i < _n; ++i)
+    _array[i] = new double[_m];
+  for(int i = 0; i < _n; ++i)
+  {
+    for(int j = 0; j < _m; ++j)
+      _array[i][j] = matrix._array[i][j];
+  }
+}
+
+Matrix::Matrix(int n, int m) : _n(n), _m(m)
+{
+  _array = new double*[n];
+  for(int i = 0; i < n; ++i)
+    _array[i] = new double[m];
+}
+
+Matrix::~Matrix()
+{
+  for(int i = 0; i < _n; ++i)
+    delete _array[i];
+  delete _array;
+}
+
+Matrix::aVector::aVector(double* arr) : elements(arr)
 {
 
+}
+
+double Matrix::aVector::operator[](int i)
+{
+  return elements[i];
+}
+
+Matrix::aVector Matrix::operator[](int i)
+{
+  return aVector(_array[i]);
 }
 
